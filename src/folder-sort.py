2d9e4048b312fee047995e62a23b9d2569647b3e
diomid <diomidtreumov@gmail.com>
2020-12-28 17:50:29 +0200

    init 2

--- src/folder-sort.py
+++ src/folder-sort.py
@@ -7,10 +7,8 @@ from watchdog.events import FileSystemEventHandler
 class Handler(FileSystemEventHandler):
     def on_modified(self, event):
         for filename in os.listdir(folder_track):
-            extention = filename.split(".")
-            print(extention)
-            print(extention)
-            if len(extention) > 1 and (extention[1].lower() == "fb2" or extention[1].lower() == "doc" or extention[1].lower() == "mp4"):
+            file_name,  extention = os.path.splitext(filename)
+            if len(extention) > 1 and extention in ext_list:
                 file = folder_track + "/" + filename
                 new_path = folder_dest + "/" + filename
                 os.rename(file, new_path)
@@ -18,6 +16,8 @@ class Handler(FileSystemEventHandler):
 
 folder_track = "/home/rox-9/trashbox"
 folder_dest = "/home/rox-9/Video"
+ext_list = [".fb2", ".mp4"]
+
 handle = Handler()
 observer = Observer()
 observer.schedule(handle, folder_track, recursive=True)
